2b0c35f2b1fe9344e4a2afda15527fff362f8fb1
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 19:13:05 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+int dv = 99;
+int Input = int.Parse(Console.ReadLine());
+if (Input < 100)
+{
+    Console.WriteLine(Input);
+}
+else
+{
+    for (int i = 100; i <= Input; i++)
+    {
+        if (i % 10 + i / 100 == 2 * ((i / 10) % 10))
+        {
+            dv++;
+        }
+    }
+    Console.WriteLine(dv);
+}
